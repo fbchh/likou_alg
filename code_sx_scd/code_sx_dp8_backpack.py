efff8367b32ab895fc1d4d1008178c8b0a88c8df
LZD-20230508PLU\Administrator <2446265827@qq.com>
2024-09-09 21:36:51 +0800

    二刷-动规之背包问题策略2-滚动数组

--- code_sx_scd/code_sx_dp8_backpack.py
+++ code_sx_scd/code_sx_dp8_backpack.py
@@ -21,7 +21,26 @@ def solve(weights, values, backpack_max_weight):
     return res[-1][-1]
 
 
+def solve1(weights, values, backpack_max_weight):
+    res = [0 for _1 in range(backpack_max_weight + 1)]
+    for i in range(1, len(res)):
+        if weights[0] <= i:
+            res[i] = values[0]
+
+    for i in range(1, len(weights)):
+        for j in range(len(res) - 1, 0, -1):
+            if weights[i] < j:
+                res[j] = max(res[j], values[i] + res[j - weights[i]])
+            elif weights[i] == j:
+                res[j] = max(res[j], values[i])
+
+    return res[-1]
+
+
 if __name__ == "__main__":
     print(solve([1, 3, 4], [15, 20, 30], 4))
     print(solve([1, 3, 2, 4], [20, 50, 32, 27], 5))
+
+    print(solve1([1, 3, 4], [15, 20, 30], 4))
+    print(solve1([1, 2, 3, 4], [20, 50, 32, 27], 5))
     ...
