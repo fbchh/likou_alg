14f48dedfa7c794e87eb6eb939fa67b96b51c624
LZD-20230508PLU\Administrator <2446265827@qq.com>
2024-10-08 23:32:28 +0800

    二刷-动规之打家劫舍II更新

--- code_sx_scd/code_sx_dptol2_robberyII.py
+++ code_sx_scd/code_sx_dptol2_robberyII.py
@@ -5,21 +5,21 @@ xxx
 
 def solve(nums):
     def inner_solve(tgt_nums):
-        len_nums = len(tgt_nums)
-        if len_nums == 0:
-            return 0
-        if len_nums <= 2:
-            return max(tgt_nums)
-
-        res = [0] * len_nums
+        inner_len_nums = len(tgt_nums)
+        res = [0] * inner_len_nums
         res[0] = tgt_nums[0]
         res[1] = max(tgt_nums[:2])
 
-        for i in range(2, len_nums):
+        for i in range(2, inner_len_nums):
             res[i] = max(res[i - 2] + tgt_nums[i], res[i - 1])
 
         return res[-1]
 
+    len_nums = len(nums)
+    if len_nums == 0:
+        return 0
+    if len_nums <= 2:
+        return max(nums)
     res_res = inner_solve(nums[:-1]), inner_solve(nums[1:])
     return max(res_res)
 
@@ -28,4 +28,5 @@ if __name__ == "__main__":
     print(solve([2, 3, 2]))
     print(solve([1, 2, 3, 1]))
     print(solve([1, 2, 3]))
+    print(solve([1]))
     ...
