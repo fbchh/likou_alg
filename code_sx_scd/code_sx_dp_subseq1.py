d2ecc9456ca4af79466cbb2aac3acc2403338dc3
LZD-20230508PLU\Administrator <2446265827@qq.com>
2024-10-17 21:35:26 +0800

    二刷-动规之最长上升子序列

---
+++ code_sx_scd/code_sx_dp_subseq1.py
@@ -0,0 +1,24 @@
+"""
+xxx
+"""
+
+
+def solve(nums):
+    len_nums = len(nums)
+    res = 1
+
+    dp = [1] * len_nums
+    for i in range(1, len_nums):
+        for j in range(i):
+            if n    ums[j] < nums[i]:
+                dp[i] = max(dp[i], dp[j] + 1)
+        res = max(res, dp[i])
+
+    return res
+
+
+if __name__ == "__main__":
+    print(solve([10, 9, 2, 5, 3, 7, 101, 18]))
+    print(solve([0, 1, 0, 3, 2, 3]))
+    print(solve([7, 7, 7, 7, 7, 7, 7]))
+    ...
